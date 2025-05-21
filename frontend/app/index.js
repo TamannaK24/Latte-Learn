e31ff42795308b7d63a807bb78abbf6c5a46224d
Tamanna Khurana <Tamanna122404@gmail.com>
2025-05-21 15:19:59 -0500

    Environment set up for frontend and backend

---
+++ frontend/app/index.js
@@ -0,0 +1,10 @@
+import { Stylesheet, Text, View} from 'react-native'
+const Home = () => {
+    return (
+        <View>
+            <Text>index</Text>
+        </View>
+    )
+}
+export default Home
+const styles = Stylesheet.create({})
